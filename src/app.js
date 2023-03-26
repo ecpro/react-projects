f26b41dbca64b3c0902f188f281ad8ec742eb43e
Piyush Ravi <piyush.ravi@postman.com>
2023-03-26 13:19:47 +0530

    jsx intro and react functiona components

--- src/app.js
+++ src/app.js
@@ -1,8 +1,41 @@
-import react from "react";
-import reactDom from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const hey = react.createElement("h1", { key: 1 }, "Hey There"),
-  there = react.createElement("h2", { key: 2 }, "I am Learning React");
+// JSX => React.createElement => HTMLElement
+const jsxHeading = (
+  <h1 id="heading" className="hey">
+    Hello React using JSX
+  </h1>
+);
 
-const root = reactDom.createRoot(document.getElementById("root"));
-root.render([hey, there]);
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Hello React using JSX Component 🚀
+  </h1>
+);
+
+const Title2 = function() {
+  return (
+    <h1 className="head" tabIndex="5">
+    Hello React Component using Old Style function 🚀
+  </h1>
+  );
+}
+
+// react component - just a function that returns a react element
+// Component Composition
+const HeadingComponent = () => (
+    <div id="container">
+      {
+        jsxHeading
+      }
+      {
+        Title2()
+      }
+      <Title />
+      <h1 className="heading">Hello React Functional Component</h1>
+    </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
